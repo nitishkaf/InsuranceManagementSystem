ef475268e37de7fe2a83215fce4094f226e8a8d8
Nitish Kafle <kaflenit@gmail.com>
2023-12-18 11:53:47 -0600

    give a hardcoded admin access

--- src/dao/UserDAOImpl.java
+++ src/dao/UserDAOImpl.java
@@ -8,6 +8,19 @@ import java.util.List;
 public class UserDAOImpl implements UserDAO {
     private List<User> users = new ArrayList<>();
 
+    public UserDAOImpl(){
+        User adminUser = new User();
+        adminUser.setUid(1);
+        adminUser.setFirstName("Admin");
+        adminUser.setLastName("User");
+        adminUser.setEmail("admin@admin.com");
+        adminUser.setUserName("admin");
+        adminUser.setPassword("admin");
+        adminUser.setRoleName("Admin");
+
+        users.add(adminUser);
+    }
+
     @Override
     public void addUser(User user){
         users.add(user);
